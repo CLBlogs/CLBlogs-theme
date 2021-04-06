c0406d11d75be463b582a4b6aeebeaf8ff01f1ae
fnsflm <fnsflm@qq.com>
2021-04-06 22:37:31 +0800

    注释release1完成不了的内容, 并修改引用的问题

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <?php
     $road = get_template_directory_uri();
     // echo $road;
+    echo "<link rel=\"stylesheet\" href=\"$road/assets/js/jquery.min.js\">";
+    echo "<link rel=\"stylesheet\" href=\"$road/assets/bootstrap/js/bootstrap.min.js\">";
     echo "<link rel=\"stylesheet\" href=\"$road/assets/bootstrap/css/bootstrap.min.css\">";
     echo "<link rel=\"stylesheet\" href=\"$road/assets/css/Navigation-Clean.css\">";
     echo "<link rel=\"stylesheet\" href=\"$road/assets/css/Navigation-with-Search-1.css\">";
@@ -50,28 +52,28 @@
     </div>
     <div id="main-contain" class="hero">
         <div id="main" class="main">
-            <div id="side_left" class="side-left card-list side-bar">
-                <div id="cnblog_b1" class="sidebar-image"></div>
-                <div id="sidebar_bh" class="sidebar-bh"></div>
-                <div class="card">
-                    <h4 class="card-title"><a href="#">排行榜标题</a></h4>
-                    <ul class="item-list">
-                        <li><span class="number highlight">1</span><a href="#">Announcing -hardening<br><br></a></li>
-                        <li><span class="number highlight">2</span><a href="#">有意思！强大的 SVG 滤镜<br><br><br></a></li>
-                        <li><span class="number highlight">3</span><a href="#">CentOS离线安装Nginx<br><br><br></a></li>
-                        <li><span class="number highlight">4</span><a href="#">图解 | 原来这就是 class<br><br></a></li>
-                    </ul>
-                </div>
-                <div class="card">
-                    <h4 class="card-title"><a href="#">排行榜标题</a></h4>
-                    <ul class="item-list">
-                        <li><span class="number highlight">1</span><a href="#">Announcing -hardening<br><br></a></li>
-                        <li><span class="number highlight">2</span><a href="#">有意思！强大的 SVG 滤镜<br><br><br></a></li>
-                        <li><span class="number highlight">3</span><a href="#">CentOS离线安装Nginx<br><br><br></a></li>
-                        <li><span class="number highlight">4</span><a href="#">图解 | 原来这就是 class<br><br></a></li>
-                    </ul>
-                </div>
-            </div>
+<!--            <div id="side_left" class="side-left card-list side-bar">-->
+<!--                <div id="cnblog_b1" class="sidebar-image"></div>-->
+<!--                <div id="sidebar_bh" class="sidebar-bh"></div>-->
+<!--                <div class="card">-->
+<!--                    <h4 class="card-title"><a href="#">排行榜标题</a></h4>-->
+<!--                    <ul class="item-list">-->
+<!--                        <li><span class="number highlight">1</span><a href="#">Announcing -hardening<br><br></a></li>-->
+<!--                        <li><span class="number highlight">2</span><a href="#">有意思！强大的 SVG 滤镜<br><br><br></a></li>-->
+<!--                        <li><span class="number highlight">3</span><a href="#">CentOS离线安装Nginx<br><br><br></a></li>-->
+<!--                        <li><span class="number highlight">4</span><a href="#">图解 | 原来这就是 class<br><br></a></li>-->
+<!--                    </ul>-->
+<!--                </div>-->
+<!--                <div class="card">-->
+<!--                    <h4 class="card-title"><a href="#">排行榜标题</a></h4>-->
+<!--                    <ul class="item-list">-->
+<!--                        <li><span class="number highlight">1</span><a href="#">Announcing -hardening<br><br></a></li>-->
+<!--                        <li><span class="number highlight">2</span><a href="#">有意思！强大的 SVG 滤镜<br><br><br></a></li>-->
+<!--                        <li><span class="number highlight">3</span><a href="#">CentOS离线安装Nginx<br><br><br></a></li>-->
+<!--                        <li><span class="number highlight">4</span><a href="#">图解 | 原来这就是 class<br><br></a></li>-->
+<!--                    </ul>-->
+<!--                </div>-->
+<!--            </div>-->
             <div id="main-flow" class="main-flow">
                 <div id="post-list" class="post-list">
                     <article id="post-item-1" class="post-item">
@@ -148,11 +150,16 @@
                     </article>
                     <article id="post-item-5" class="post-item">
                         <section id="post-item-body-5" class="post-item-body">
-                            <div id="post-item-text-5" class="post-item-text"><a class="post-item-title"
-                                                                                 href="#">博客标题</a>
-                                <p class="post-item-summary"><a href="#"><img class="avatar"
-                                                                              src="http://106.54.169.139/wp-content/themes/Clblogs_theme_home/assets/img/photo_test.png">前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
-                                        Dubbo架构 概述 Dubbo是阿里巴巴公司开源的一个高性能优秀的服务框架，使得应用可通过高性能的RPC 实现服务<br></a></p>
+                            <div id="post-item-text-5" class="post-item-text">
+                                <a class="post-item-title" href="#">博客标题</a>
+                                <p class="post-item-summary">
+                                    <a href="#">
+                                        <?php echo "<img class=\"avatar\" src=\"$road/assets/img/photo_test.png\">"; ?>
+                                        前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
+                                        Dubbo架构 概述 Dubbo是阿里巴巴公司开源的一个高性能优秀的服务框架，使得应用可通过高性能的RPC 实现服务
+                                        <br>
+                                    </a>
+                                </p>
                             </div>
                             <footer id="post-item-foot-5" class="post-item-foot"><a class="post-item-author"
                                                                                     href="#"><span><span
@@ -166,29 +173,26 @@
                     </article>
                 </div>
             </div>
-            <div id="side_right" class="side_right">
-                <div class="list-box">
-                    <div>
-                        <div class="list-group">
-                            <ul class="list-nav">
-                                <li class="list-right-item"><a href="#">Java</a></li>
-                                <li class="list-right-item"><a href="#">Python</a></li>
-                                <li class="list-right-item"><a href="#">C/C++</a></li>
-                                <li class="list-right-item"><a href="#">数据库</a></li>
-                                <li class="list-right-item"><a href="#">JavaScript</a></li>
-                                <li class="list-right-item"><a href="#">HTML</a></li>
-                                <li class="list-right-item"><a href="#">CSS</a></li>
-                                <li class="list-right-item"><a href="#">PHP</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<!--            <div id="side_right" class="side_right">-->
+<!--                <div class="list-box">-->
+<!--                    <div>-->
+<!--                        <div class="list-group">-->
+<!--                            <ul class="list-nav">-->
+<!--                                <li class="list-right-item"><a href="#">Java</a></li>-->
+<!--                                <li class="list-right-item"><a href="#">Python</a></li>-->
+<!--                                <li class="list-right-item"><a href="#">C/C++</a></li>-->
+<!--                                <li class="list-right-item"><a href="#">数据库</a></li>-->
+<!--                                <li class="list-right-item"><a href="#">JavaScript</a></li>-->
+<!--                                <li class="list-right-item"><a href="#">HTML</a></li>-->
+<!--                                <li class="list-right-item"><a href="#">CSS</a></li>-->
+<!--                                <li class="list-right-item"><a href="#">PHP</a></li>-->
+<!--                            </ul>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
         </div>
     </div>
 </div>
-<script src="http://106.54.169.139/wp-content/themes/Clblogs_theme_home/assets/js/jquery.min.js"></script>
-<script src="http://106.54.169.139/wp-content/themes/Clblogs_theme_home/assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
-
 </html>
