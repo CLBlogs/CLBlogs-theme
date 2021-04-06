e4c930a9ce753d676873a1a44ab4154c3cf5536f
fnsflm <fnsflm@qq.com>
2021-04-06 23:35:52 +0800

    删除依赖遗留的图片问题, 衔接登录/注册界面
    搜索栏要调到导航栏里...可惜我不会

--- index.php
+++ index.php
@@ -41,46 +41,49 @@
                                     class="form-control search-field" type="search" id="search-field" name="search">
                         </div>
                     </form>
-                    <a class="btn btn-default navbar-btn navbar-right action-button" role="button" href="#">Action </a>
+                    <a class="btn btn-default navbar-btn navbar-right action-button" role="button" href="#">搜索</a>
                 </div>
             </div>
         </nav>
         <ul class="nav nav-tabs">
-            <li><a id="denglu" href="#">登录</a></li>
-            <li><a id="zhuce" href="#">注册</a></li>
+            <?php
+            echo "<li><a id=\"denglu\" href=\"$road/login.php\">登录/注册</a></li>";
+            ?>
         </ul>
     </div>
     <div id="main-contain" class="hero">
         <div id="main" class="main">
-<!--            <div id="side_left" class="side-left card-list side-bar">-->
-<!--                <div id="cnblog_b1" class="sidebar-image"></div>-->
-<!--                <div id="sidebar_bh" class="sidebar-bh"></div>-->
-<!--                <div class="card">-->
-<!--                    <h4 class="card-title"><a href="#">排行榜标题</a></h4>-->
-<!--                    <ul class="item-list">-->
-<!--                        <li><span class="number highlight">1</span><a href="#">Announcing -hardening<br><br></a></li>-->
-<!--                        <li><span class="number highlight">2</span><a href="#">有意思！强大的 SVG 滤镜<br><br><br></a></li>-->
-<!--                        <li><span class="number highlight">3</span><a href="#">CentOS离线安装Nginx<br><br><br></a></li>-->
-<!--                        <li><span class="number highlight">4</span><a href="#">图解 | 原来这就是 class<br><br></a></li>-->
-<!--                    </ul>-->
-<!--                </div>-->
-<!--                <div class="card">-->
-<!--                    <h4 class="card-title"><a href="#">排行榜标题</a></h4>-->
-<!--                    <ul class="item-list">-->
-<!--                        <li><span class="number highlight">1</span><a href="#">Announcing -hardening<br><br></a></li>-->
-<!--                        <li><span class="number highlight">2</span><a href="#">有意思！强大的 SVG 滤镜<br><br><br></a></li>-->
-<!--                        <li><span class="number highlight">3</span><a href="#">CentOS离线安装Nginx<br><br><br></a></li>-->
-<!--                        <li><span class="number highlight">4</span><a href="#">图解 | 原来这就是 class<br><br></a></li>-->
-<!--                    </ul>-->
-<!--                </div>-->
-<!--            </div>-->
+            <!--            <div id="side_left" class="side-left card-list side-bar">-->
+            <!--                <div id="cnblog_b1" class="sidebar-image"></div>-->
+            <!--                <div id="sidebar_bh" class="sidebar-bh"></div>-->
+            <!--                <div class="card">-->
+            <!--                    <h4 class="card-title"><a href="#">排行榜标题</a></h4>-->
+            <!--                    <ul class="item-list">-->
+            <!--                        <li><span class="number highlight">1</span><a href="#">Announcing -hardening<br><br></a></li>-->
+            <!--                        <li><span class="number highlight">2</span><a href="#">有意思！强大的 SVG 滤镜<br><br><br></a></li>-->
+            <!--                        <li><span class="number highlight">3</span><a href="#">CentOS离线安装Nginx<br><br><br></a></li>-->
+            <!--                        <li><span class="number highlight">4</span><a href="#">图解 | 原来这就是 class<br><br></a></li>-->
+            <!--                    </ul>-->
+            <!--                </div>-->
+            <!--                <div class="card">-->
+            <!--                    <h4 class="card-title"><a href="#">排行榜标题</a></h4>-->
+            <!--                    <ul class="item-list">-->
+            <!--                        <li><span class="number highlight">1</span><a href="#">Announcing -hardening<br><br></a></li>-->
+            <!--                        <li><span class="number highlight">2</span><a href="#">有意思！强大的 SVG 滤镜<br><br><br></a></li>-->
+            <!--                        <li><span class="number highlight">3</span><a href="#">CentOS离线安装Nginx<br><br><br></a></li>-->
+            <!--                        <li><span class="number highlight">4</span><a href="#">图解 | 原来这就是 class<br><br></a></li>-->
+            <!--                    </ul>-->
+            <!--                </div>-->
+            <!--            </div>-->
             <div id="main-flow" class="main-flow" style="MARGIN-RIGHT: auto; MARGIN-LEFT: auto; ">
                 <div id="post-list" class="post-list">
                     <article id="post-item-1" class="post-item">
                         <section id="post-item-body-1" class="post-item-body">
                             <div id="post-item-text-1" class="post-item-text"><a class="post-item-title"
                                                                                  href="#">博客标题</a>
-                                <p class="post-item-summary"><a href="#"><?php echo "<img class=\"avatar\" src=\"$road/img/photo_test.png\">"; ?>前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
+                                <p class="post-item-summary"><a
+                                            href="#"><?php echo "<img class=\"avatar\" src=\"$road/img/photo_test.png\">"; ?>
+                                        前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
                                         Dubbo架构 概述 Dubbo是阿里巴巴公司开源的一个高性能优秀的服务框架，使得应用可通过高性能的RPC 实现服务<br></a></p>
                             </div>
                             <footer id="post-item-foot-1" class="post-item-foot"><a class="post-item-author"
@@ -97,7 +100,9 @@
                         <section id="post-item-body-1" class="post-item-body">
                             <div id="post-item-text-1" class="post-item-text"><a class="post-item-title"
                                                                                  href="#">博客标题</a>
-                                <p class="post-item-summary"><a href="#"><?php echo "<img class=\"avatar\" src=\"$road/img/photo_test.png\">"; ?>前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
+                                <p class="post-item-summary"><a
+                                            href="#"><?php echo "<img class=\"avatar\" src=\"$road/img/photo_test.png\">"; ?>
+                                        前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
                                         Dubbo架构 概述 Dubbo是阿里巴巴公司开源的一个高性能优秀的服务框架，使得应用可通过高性能的RPC 实现服务<br></a></p>
                             </div>
                             <footer id="post-item-foot-1" class="post-item-foot"><a class="post-item-author"
@@ -114,7 +119,9 @@
                         <section id="post-item-body-1" class="post-item-body">
                             <div id="post-item-text-1" class="post-item-text"><a class="post-item-title"
                                                                                  href="#">博客标题</a>
-                                <p class="post-item-summary"><a href="#"><?php echo "<img class=\"avatar\" src=\"$road/img/photo_test.png\">"; ?>前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
+                                <p class="post-item-summary"><a
+                                            href="#"><?php echo "<img class=\"avatar\" src=\"$road/img/photo_test.png\">"; ?>
+                                        前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
                                         Dubbo架构 概述 Dubbo是阿里巴巴公司开源的一个高性能优秀的服务框架，使得应用可通过高性能的RPC 实现服务<br></a></p>
                             </div>
                             <footer id="post-item-foot-1" class="post-item-foot"><a class="post-item-author"
@@ -131,7 +138,9 @@
                         <section id="post-item-body-1" class="post-item-body">
                             <div id="post-item-text-1" class="post-item-text"><a class="post-item-title"
                                                                                  href="#">博客标题</a>
-                                <p class="post-item-summary"><a href="#"><?php echo "<img class=\"avatar\" src=\"$road/img/photo_test.png\">"; ?>前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
+                                <p class="post-item-summary"><a
+                                            href="#"><?php echo "<img class=\"avatar\" src=\"$road/img/photo_test.png\">"; ?>
+                                        前面我们研究了RPC的原理，市面上有很多基于RPC思想实现的框架，比如有Dubbo。今天就从Dubbo的SPI机制、服务注册与发现源码及网络通信过程去深入剖析下Dubbo。
                                         Dubbo架构 概述 Dubbo是阿里巴巴公司开源的一个高性能优秀的服务框架，使得应用可通过高性能的RPC 实现服务<br></a></p>
                             </div>
                             <footer id="post-item-foot-1" class="post-item-foot"><a class="post-item-author"
@@ -146,24 +155,24 @@
                     </article>
                 </div>
             </div>
-<!--            <div id="side_right" class="side_right">-->
-<!--                <div class="list-box">-->
-<!--                    <div>-->
-<!--                        <div class="list-group">-->
-<!--                            <ul class="list-nav">-->
-<!--                                <li class="list-right-item"><a href="#">Java</a></li>-->
-<!--                                <li class="list-right-item"><a href="#">Python</a></li>-->
-<!--                                <li class="list-right-item"><a href="#">C/C++</a></li>-->
-<!--                                <li class="list-right-item"><a href="#">数据库</a></li>-->
-<!--                                <li class="list-right-item"><a href="#">JavaScript</a></li>-->
-<!--                                <li class="list-right-item"><a href="#">HTML</a></li>-->
-<!--                                <li class="list-right-item"><a href="#">CSS</a></li>-->
-<!--                                <li class="list-right-item"><a href="#">PHP</a></li>-->
-<!--                            </ul>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
+            <!--            <div id="side_right" class="side_right">-->
+            <!--                <div class="list-box">-->
+            <!--                    <div>-->
+            <!--                        <div class="list-group">-->
+            <!--                            <ul class="list-nav">-->
+            <!--                                <li class="list-right-item"><a href="#">Java</a></li>-->
+            <!--                                <li class="list-right-item"><a href="#">Python</a></li>-->
+            <!--                                <li class="list-right-item"><a href="#">C/C++</a></li>-->
+            <!--                                <li class="list-right-item"><a href="#">数据库</a></li>-->
+            <!--                                <li class="list-right-item"><a href="#">JavaScript</a></li>-->
+            <!--                                <li class="list-right-item"><a href="#">HTML</a></li>-->
+            <!--                                <li class="list-right-item"><a href="#">CSS</a></li>-->
+            <!--                                <li class="list-right-item"><a href="#">PHP</a></li>-->
+            <!--                            </ul>-->
+            <!--                        </div>-->
+            <!--                    </div>-->
+            <!--                </div>-->
+            <!--            </div>-->
         </div>
     </div>
 </div>
