4b05090878afee50123863750d34f5d9b63a4b93
fnsflm <fnsflm@qq.com>
2021-04-27 12:32:34 +0800

    use wpdb

--- comments.php
+++ comments.php
@@ -2,23 +2,19 @@
 <head>
     <meta charset="UTF-8">
     <link rel='stylesheet' id='skeleton-css'
-          href='http://test2.fnsflm.xyz:8190/wp-content/themes/ninja-shadow/assets/css/skeleton.css?ver=5.7'
+          href='https://cdn.jsdelivr.net/gh/fnsflm/myPicbed/clblogs/css/skeleton.css'
           type='text/css' media='all'/>
     <link rel='stylesheet' id='base-css'
-          href='http://test2.fnsflm.xyz:8190/wp-content/themes/ninja-shadow/assets/css/base.css?ver=5.7' type='text/css'
+          href='https://cdn.jsdelivr.net/gh/fnsflm/myPicbed/clblogs/css/base.css' type='text/css'
           media='all'/>
     <link rel='stylesheet' id='theme-style-css'
-          href='http://test2.fnsflm.xyz:8190/wp-content/themes/ninja-shadow/assets/css/theme-style.css?ver=5.7'
+          href='https://cdn.jsdelivr.net/gh/fnsflm/myPicbed/clblogs/css/theme-style.css'
           type='text/css' media='all'/>
     <?php
-    // require '../../../wp-blog-header.php';
     global $wpdb;
     $road = get_template_directory_uri();
     $emotion = $road . "/img/emotion/";
-    //echo $road;
     $articleId = get_the_ID();
-    //echo $id;
-
     ?>
 
 </head>
@@ -31,26 +27,29 @@
 
                     <!-- 连接数据库，动态加载评论 -->
                     <?php
-                    //连接blog数据库
-                    $conn = mysqli_connect("localhost", "test3_fnsflm_xy", "65ksKYZxKX");
-                    if (!$conn) {
-                        die("数据库连接失败！");
-                    }
-                    mysqli_select_db($conn, "test3_fnsflm_xy");
-                    mysqli_query($conn, "set names utf8");
+                    // 连接blog数据库
+                    // $conn = mysqli_connect("localhost", "test3_fnsflm_xy", "65ksKYZxKX");
+//                    if (!$conn) {
+//                        die("数据库连接失败！");
+//                    }
+                    // mysqli_select_db($conn, "test3_fnsflm_xy");
+                    // mysqli_query($conn, "set names utf8");
                     //查询获取wp_comments表中所有评论数据
                     $sql = "select * from wp_comments where comment_post_ID=" . $articleId;
-                    $rst = mysqli_query($conn, $sql);
-                    $rows = mysqli_num_rows($rst);
-
+                    // $rst = mysqli_query($conn, $sql);
+                    // $rows = mysqli_num_rows($rst);
+                    $rst = $wpdb->get_results($sql);
+                    $rows = $wpdb->num_rows;
                     ?>
                     <h2 class="comments-title">
                         <span>有<?php echo $rows ?>条评论</span>
                     </h2>
                     <?php
                     //动态加载所有0级评论
-                    while ($arr = mysqli_fetch_assoc($rst)) {
-                        if ($arr['comment_parent'] == 0) {
+                    // while ($arr = mysqli_fetch_assoc($rst)) {
+                        foreach ($rst as $arr){
+                        // if ($arr['comment_parent'] == 0) {
+                        if ($arr->comment_parent == 0){
                             ?>
 
                             <!-- 评论列表 -->
@@ -60,17 +59,17 @@
                                         <footer class="comment-meta">
                                             <div class="comment-author vcard">
                                                 <b class="fn"><a href='#' rel='external nofollow ugc'
-                                                                 class='url'><?php echo $arr['comment_author']; ?></a></b><span
+                                                                 class='url'><?php echo $arr->comment_author ?></a></b><span
                                                         class="says">说道：</span>
                                             </div><!-- .comment-author -->
                                             <div class="comment-metadata">
-                                                <time datetime="2021-03-07T15:48:41+00:00"><?php echo $arr['comment_date']; ?></time>
+                                                <time datetime="2021-03-07T15:48:41+00:00"><?php echo $arr->comment_date ?></time>
                                             </div><!-- .comment-metadata -->
                                         </footer><!-- .comment-meta -->
 
                                         <div class="comment-content">
                                             <?php
-                                            $comment_content = $arr['comment_content'];
+                                            $comment_content = $arr->comment_content;
                                             while ($comment_content) {
                                                 if ($i = strpos($comment_content, ".gif")) {
                                                     $l = strpos($comment_content, "&");
@@ -90,30 +89,31 @@
                                         </div><!-- .comment-content -->
                                         <div class="reply"><a rel='nofollow' class='comment-reply-link'
                                                               href='#reply-title'
-                                                              onclick="replication(<?php echo $arr['comment_ID']; ?>,'<?php echo $arr['comment_author']; ?>')">回复</a>
+                                                              onclick="replication(<?php echo $arr->comment_ID; ?>,'<?php echo $arr->comment_author; ?>')">回复</a>
                                         </div>
 
                                         <!-- 动态加载回复 -->
                                         <?php
                                         //动态加载所有子评论
-                                        $subRst = mysqli_query($conn, $sql);
-                                        while ($subArr = mysqli_fetch_assoc($subRst)) {
-                                            if ($subArr['comment_parent'] == $arr['comment_ID']) {
-                                                ?>
+                                        // $subRst = mysqli_query($conn, $sql);
+                                        $subRst = $wpdb->get_results($sql);
+                                        foreach ($subRst as $subArr){
+                                                if ($subArr->comment_parent == $arr -> comment_ID)   {
+                                            ?>
                                                 <footer class="comment-meta">
                                                     <div class="comment-author vcard">
                                                         <b class="fn"><a href='#' rel='external nofollow ugc'
-                                                                         class='url'>&nbsp;&nbsp;&nbsp;<?php echo $subArr['comment_author']; ?></a></b><span
+                                                                         class='url'>&nbsp;&nbsp;&nbsp;<?php echo $subArr->comment_author; ?></a></b><span
                                                                 class="says">说道：</span>
                                                     </div><!-- .comment-author -->
                                                     <div class="comment-metadata">
                                                         <time datetime="2021-03-07T15:48:41+00:00">
-                                                            &nbsp;&nbsp;&nbsp;<?php echo $subArr['comment_date']; ?></time>
+                                                            &nbsp;&nbsp;&nbsp;<?php echo $subArr->comment_date; ?></time>
                                                     </div><!-- .comment-metadata -->
                                                 </footer><!-- .comment-meta -->
                                                 <div class="comment-content">
                                                     &nbsp;&nbsp;&nbsp; <?php
-                                                    $sub_comment_content = $subArr['comment_content'];
+                                                    $sub_comment_content = $subArr->comment_content;
                                                     while ($sub_comment_content) {
                                                         if ($m = strpos($sub_comment_content, ".gif")) {
                                                             $n = strpos($sub_comment_content, "&");
@@ -136,7 +136,7 @@
                                     </article><!-- .comment-body -->
                                 </li><!-- #comment-## -->
                             </ol><!-- .comment-list -->
-                        <?php }
+                        <? }
                     } ?>
 
                     <!-- 发表评论/回复 -->
@@ -180,7 +180,6 @@
 <script type="text/javascript">
     var $test = window.location.href;
     document.getElementById("currentUrl").value=$test;
-    document.getElementById("articleId").value=
     function replication(id, name) {
         document.getElementById("reply-title").innerHTML = "回复给" + name;
         document.getElementById("huifuid").value = id;
--- controller.php
+++ controller.php
@@ -1,22 +1,28 @@
 <?php
 //连接blog数据库
-$conn = mysqli_connect("localhost", "test3_fnsflm_xy", "65ksKYZxKX");
-if (!$conn) {
-    die("数据库连接失败！");
-}
-mysqli_select_db($conn, "test3_fnsflm_xy");
-mysqli_query($conn, "set names utf8");
-
+// $conn = mysqli_connect("localhost", "test3_fnsflm_xy", "65ksKYZxKX");
+//if (!$conn) {
+//    die("数据库连接失败！");
+//}
+//mysqli_select_db($conn, "test3_fnsflm_xy");
+//mysqli_query($conn, "set names utf8");
+require '../../../wp-blog-header.php';
+global $wpdb;
 $arr = $_POST;
-$nextUrl=$arr['cUrl'];
+$nextUrl = $arr['cUrl'];
 if (!$arr['comment']) {
     echo "<script>location.href='$nextUrl';</script>";
 } else {
     if (is_numeric($arr['hfId'])) {
         //将子评论插入wp_comments数据库
-        $sql = "insert into wp_comments set comment_post_ID='" . $arr['aId'] . "',comment_parent='" . $arr['hfId'] . "',comment_author='jiangchen',comment_content='" . $arr['comment'] . "',comment_date='" . date("Y-m-d H:i:s") . "'";
-        $rst = mysqli_query($conn, $sql);
-
+        // $sql = "insert into wp_comments set comment_post_ID='" . $arr['aId'] . "',comment_parent='" . $arr['hfId'] . "',comment_author='jiangchen',comment_content='" . $arr['comment'] . "',comment_date='" . date("Y-m-d H:i:s") . "'";
+        $rst = $wpdb->insert("wp_comments", array(
+            "comment_post_ID" => $arr['aId'],
+            "comment_parent" => $arr['hfId'],
+            "comment_author" => "jiangchen",
+            "comment_content" => $arr['comment'],
+            "comment_date" => date("Y-m-d H:i:s")
+        ));
         if ($rst) {
             echo "<script>location.href='$nextUrl';</script>";
         } else {
@@ -24,8 +30,14 @@ if (!$arr['comment']) {
         }
     } else {
         //将0级评论插入wp_comments数据库
-        $sql = "insert into wp_comments set comment_post_ID='" . $arr['aId'] . "',comment_parent='0',comment_author='jiangchen',comment_content='" . $arr['comment'] . "',comment_date='" . date("Y-m-d H:i:s") . "'";
-        $rst = mysqli_query($conn, $sql);
+        // $sql = "insert into wp_comments set comment_post_ID='" . $arr['aId'] . "',comment_parent='0',comment_author='jiangchen',comment_content='" . $arr['comment'] . "',comment_date='" . date("Y-m-d H:i:s") . "'";
+        $rst = $wpdb->insert("wp_comments", array(
+            "comment_post_ID" => $arr['aId'],
+            "comment_parent" => 0,
+            "comment_author" => "jiangchen",
+            "comment_content" => $arr['comment'],
+            "comment_date" => date("Y-m-d H:i:s")
+        ));
 
         if ($rst) {
             echo "<script>location.href='$nextUrl';</script>";
@@ -34,4 +46,3 @@ if (!$arr['comment']) {
         }
     }
 }
-?>
