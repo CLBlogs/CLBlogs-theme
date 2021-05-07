00d66215b5672c3866e814c1090439e6dc0169db
jiangchen21 <2373446900@qq.com>
2021-05-07 20:57:55 +0800

    commit

--- controller.php
+++ controller.php
@@ -1,36 +1,44 @@
 <?php
-    //连接blog数据库
-    $conn=mysqli_connect("localhost","root","123456");
-    if(!$conn){
-        die("数据库连接失败！");
-    }
-    mysqli_select_db($conn,"blog");
-    mysqli_query($conn,"set names utf8");
+require '../../../wp-blog-header.php';
+global $wpdb;
+session_start();
+if(!empty($_SESSION['user_id'])){
+    echo "<script>location.href='login.php';</script>";
+}
+else{
+    $arr = $_POST;
+    $nextUrl = $arr['cUrl'];
+    if (!$arr['comment']) {
+        echo "<script>location.href='$nextUrl';</script>";
+    } else {
+        if (is_numeric($arr['hfId'])) {
+            $rst = $wpdb->insert("wp_comments", array(
+                "comment_post_ID" => $arr['aId'],
+                "comment_parent" => $arr['hfId'],
+                "comment_author" => "jiangchen",
+                "comment_content" => $arr['comment'],
+                "comment_date" => date("Y-m-d H:i:s")
+            ));
+            if ($rst) {
+                echo "<script>location.href='$nextUrl';</script>";
+            } else {
+                echo "留言失败";
+            }
+        } else {
 
-    $arr=$_POST;
-    if(!$arr['comment']){
-    echo "<script>location.href='comments.php';</script>";
-}else{
-    if(is_numeric($arr['hfId'])) {
-        //将子评论插入wp_comments数据库
-        $sql="insert into wp_comments set comment_parent='".$arr['hfId']."',comment_author='jiangchen',comment_content='".$arr['comment']."',comment_date='".date("Y-m-d H:i:s")."'";
-        $rst=mysqli_query($conn,$sql);
+            $rst = $wpdb->insert("wp_comments", array(
+                "comment_post_ID" => $arr['aId'],
+                "comment_parent" => 0,
+                "comment_author" => "jiangchen",
+                "comment_content" => $arr['comment'],
+                "comment_date" => date("Y-m-d H:i:s")
+            ));
 
-        if($rst){
-            echo "<script>location.href='comments.php';</script>";
-        }else{
-            echo "留言失败";
-        }
-    }else{
-        //将0级评论插入wp_comments数据库
-        $sql="insert into wp_comments set comment_parent='0',comment_author='jiangchen',comment_content='".$arr['comment']."',comment_date='".date("Y-m-d H:i:s")."'";
-        $rst=mysqli_query($conn,$sql);
-
-        if($rst){
-            echo "<script>location.href='comments.php';</script>";
-        }else{
-            echo "留言";
+            if ($rst) {
+                echo "<script>location.href='$nextUrl';</script>";
+            } else {
+                echo "留言";
+            }
         }
     }
 }
-?>
