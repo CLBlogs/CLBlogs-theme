46bbbab689be4f599a298e31ecfed20c8b111c94
hehelz <80251315+hehelz@users.noreply.github.com>
2021-06-08 12:42:26 +0800

    Add files via upload

---
+++ functions/loginout.php
@@ -0,0 +1,13 @@
+<?php
+ob_start();
+require '../../../../wp-blog-header.php';
+global $wpdb;
+
+$road = get_template_directory_uri();
+setcookie("user_login","", time() + 3600, '/');
+setcookie("user_pass","", time() + 3600, '/');
+setcookie("PHPSESSID","", time() + 3600, '/');
+setcookie("user_id","", time() + 3600, '/');
+echo "<script>
+            window.location.href=\"http://test5.fnsflm.xyz:8193/\";
+      </script>";
